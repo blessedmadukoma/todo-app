045126baf6532c6d4cc503be78c46575e9cb4fbb
blessedmadukoma <blessedmadukoma@gmail.com>
2023-01-09 00:15:01 +0100

    set up project

---
+++ web/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Login from './pages/Login.vue';
+
+</script>
+
+<template>
+  <Login />
+</template>
+
+<style scoped>
+
+</style>
---
+++ web/src/pages/Login.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const email = ref("")
+const password = ref("")
+</script>
+
+<template>
+ <input v-model="email" type="email">
+ <input v-model="password" type="password">
+</template>
